c9ac11ca7c6ba8d7728f95ce56e5264d7dae6a0a
Константин Медведев <konstantinmedvedev0@gmail.com>
2020-03-26 20:09:28 +0400

    add call methods

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 class Airplanes{
@@ -19,8 +20,26 @@ class Mig extends Airplanes {
 class Tu_154 extends Airplanes {
 
 }
+
+$mig1 = new Mig();
+$mig1->Takeoff();
+$mig1->Landing();
+$mig1->Attack();
+
+$tu1 = new Tu_154();
+$tu1->Takeoff();
+$tu1->Landing();
+
+$airplane1 = new Airplanes();
+$airplane1->Landing();
+$airplane1->Takeoff();
+
+
 $mig = get_class_methods(Mig::class);
 print_r($mig);
 echo "<br>";
 $tu = get_class_methods(Tu_154::class);
 print_r($tu) ;
+
+?>
+
